2d5df578955387f515a0380a12ff72bddf745169
Andrewlittle97 <andrewlittle@mail.weber.edu>
2017-03-22 17:32:09 +0000

    Creighton_hw7_task2.c

---
+++ Creighton_hw7_task2.c
@@ -0,0 +1,41 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Creighton_hw7_task2.c
+ *       	Usage:  ./Creighton_hw7_task2.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/22/2017 05:03:31 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Andrew Little (), andrewlittle@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+
+/* Function Prototypes */
+void MaxMin(int numvals, int vals[], int* min, int* max);
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	printf("Generating random list\n")
+		
+	printf("This list is: %d %d %d %d %d %d %d %d %d %d");
+	
+	printf("The maximum value is: %d\n");
+
+	printf("The minimum value is: %d\n");
+	return 0;
+}
+
+
+/* Function Defenitions */
+void MaxMin(int numvals, int vals[], int* min, int* max)
+{
+	rand()
+	srand()
+}
+
+
